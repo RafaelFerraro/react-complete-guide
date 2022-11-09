9deff23799d900cfff0a842ecd0822f6c34b2119
Rafael Guimarães <rafael.guimaraes@sequra.es>
2022-11-09 17:57:26 +0100

    Adding exercise to extract expenses into a component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -11,12 +11,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenses.map((expense) => {
-        return <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}/>
-      })};
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
