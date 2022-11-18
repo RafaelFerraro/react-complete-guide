53cd3a2b58edb6a2f27817e3712ae7dc19bd465c
Rafael Guimarães <rafael.guimaraes@sequra.es>
2022-11-18 16:54:21 +0100

    Removing example code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card.js';
 import './ExpenseItem.css';
 
 const ExpenseItem = (expense) => {
-  const [title, setTitle] = useState(expense.title);
-
-  const changeTitle = () => {
-    setTitle("New title");
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
-        <button onClick={changeTitle}>Change title</button>
+        <button>Change title</button>
       </div>
     </Card>
   );
